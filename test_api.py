8b9fe8e3a2ede44ea394f8519c715cf85c224401
Apolinapolis <larra12@ya.ru>
2025-03-09 11:17:00 +0300

    get / post methods added

--- test_api.py
+++ test_api.py
@@ -1,58 +1,57 @@
 import requests
 from jsonschema import validate
+from schemas import *
 
 
-GET = "?page=2"
+base_url = 'https://reqres.in/'
+get_route = base_url + 'api/users'
 
 
+class TestGetApi:
 
-post_schema = {
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "type": "object",
-  "properties": {
-    "name": {
-      "type": "string"
-    },
-    "job": {
-      "type": "string"
-    },
-    "updatedAt": {
-      "type": "string"
-    }
-  },
-  "required": [
-    "name",
-    "job",
-    "updatedAt"
-  ]
-}
+    def test_get_status_code(self):
+      response_code = requests.get(get_route).status_code
+      assert response_code == 200
 
-def test_api_post():
+    def test_get_schema(self):
+      response=requests.get(get_route).json()
+      validate(response, get_schema)
 
-  base_url = 'https://reqres.in/api/users'
+
+class TestPostApi:
+  post_route = base_url + 'api/register'
+
   payload = {
-    "name": "morpheus",
-    "job": "leader"
+    "email": "eve.holt@reqres.in",
+    "password": "testPass"
   }
 
-  response = requests.post(base_url, data=payload)
-  assert response.status_code == 201
+  def test_post_status_code(self):
+    current_status_code = requests.post(self.post_route, self.payload).status_code
+    assert  current_status_code == 200
+
+  def test_post_without_email(self):
+    new_body = self.payload.copy()
+    del new_body['email']
+
+    current_status_code = requests.post(self.post_route, new_body).status_code
+    current_response_data = requests.post(self.post_route, new_body).json()
+
+    assert current_status_code == 400 and current_response_data['error'] == 'Missing email or username'
+
+  def test_post_without_pass(self):
+    new_body = self.payload.copy()
+    del new_body['password']
+
+    current_status_code = requests.post(self.post_route, new_body).status_code
+    current_response_data = requests.post(self.post_route, new_body).json()
+
+    assert current_status_code == 400 and current_response_data['error'] == 'Missing password'
+
+  def test_post_response_schema(self):
+    response = requests.post(self.post_route, self.payload).json()
+    assert validate(response, post_schema2)
 
 
 
-def test_api_put():
-  base_url = "https://reqres.in/api/users/2"
-  new_payload = {
-    "name": "morpheus",
-    "job": "zion developer"
-}
-
-  response = requests.put(base_url, data = new_payload)
-  body = response.json()
-
-  assert response.status_code == 200
-  assert body['job'] == "zion developer"
-
-  validate(body,schema=post_schema)
-
-
+# validate - получаю ошибку соответствия схем
